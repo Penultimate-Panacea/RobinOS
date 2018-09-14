0512792ffcf722f087b0b17449606d6bfc7ecd2f
Rian <Penultimate-Panacea@users.noreply.github.com>
2018-09-14 13:33:01 -0400

    Added skeleton for Strands

--- Strand.cpp
+++ Strand.cpp
@@ -3,3 +3,65 @@
 //
 
 #include "Strand.hpp"
+
+unsigned long Strand::getThreadId() const {
+    return threadId;
+}
+
+void Strand::setThreadId(unsigned long threadId) {
+    Strand::threadId = threadId;
+}
+
+unsigned char Strand::getPriority() const {
+    return priority;
+}
+
+void Strand::setPriority(unsigned char priority) {
+    Strand::priority = priority;
+}
+
+unsigned long Strand::getExecutionTime() const {
+    return executionTime;
+}
+
+void Strand::setExecutionTime(unsigned long executionTime) {
+    Strand::executionTime = executionTime;
+}
+
+unsigned char Strand::getAlertStatus() const {
+    return alertStatus;
+}
+
+void Strand::setAlertStatus(unsigned char alertStatus) {
+    Strand::alertStatus = alertStatus;
+}
+
+unsigned int Strand::getSuspensionCount() const {
+    return suspensionCount;
+}
+
+void Strand::setSuspensionCount(unsigned int suspensionCount) {
+    Strand::suspensionCount = suspensionCount;
+}
+
+const unsigned long Strand::getHostProcessId() const {
+    return hostProcessId;
+}
+
+unsigned char Strand::getExitStatus() const {
+    return exitStatus;
+}
+
+void Strand::setExitStatus(unsigned char exitStatus) {
+    Strand::exitStatus = exitStatus;
+}
+
+Strand::Strand(unsigned long threadId, unsigned char priority, unsigned long executionTime, unsigned char alertStatus,
+               unsigned int suspensionCount, const unsigned long hostProcessId) : threadId(threadId),
+                                                                                  priority(priority),
+                                                                                  executionTime(executionTime),
+                                                                                  alertStatus(alertStatus),
+                                                                                  suspensionCount(suspensionCount),
+                                                                                  hostProcessId(hostProcessId) {}
+
+Strand::~Strand() = default;
--- Strand.hpp
+++ Strand.hpp
@@ -2,6 +2,8 @@
 // Created by Rian on 9/14/2018.
 //
 
+
+
 #ifndef ROBINOS_STRAND_HPP
 #define ROBINOS_STRAND_HPP
 
@@ -9,7 +11,43 @@
 class Strand {
 private:
     unsigned long threadId;
-    unsigned
+    unsigned char priority;
+    unsigned long executionTime;
+    unsigned char alertStatus;
+    unsigned int suspensionCount;
+    const unsigned long hostProcessId;
+    unsigned char exitStatus;
+public:
+    unsigned long getThreadId() const;
+
+    void setThreadId(unsigned long threadId);
+
+    unsigned char getPriority() const;
+
+    void setPriority(unsigned char priority);
+
+    unsigned long getExecutionTime() const;
+
+    void setExecutionTime(unsigned long executionTime);
+
+    unsigned char getAlertStatus() const;
+
+    void setAlertStatus(unsigned char alertStatus);
+
+    unsigned int getSuspensionCount() const;
+
+    void setSuspensionCount(unsigned int suspensionCount);
+
+    const unsigned long getHostProcessId() const;
+
+    unsigned char getExitStatus() const;
+
+    void setExitStatus(unsigned char exitStatus);
+
+    Strand(unsigned long threadId, unsigned char priority, unsigned long executionTime, unsigned char alertStatus,
+           unsigned int suspensionCount, const unsigned long hostProcessId);
+
+    virtual ~Strand();
 };
 
 
