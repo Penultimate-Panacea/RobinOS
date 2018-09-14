a823e45fbfec7283a219cb7574f0da4ecd0f4d0f
Rian <Penultimate-Panacea@users.noreply.github.com>
2018-09-14 09:44:04 -0400

    Added CodeObject and Process Class

---
+++ CodeObject.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Rian on 9/14/2018.
+//
+
+#ifndef ROBINOS_CODEOBJECT_HPP
+#define ROBINOS_CODEOBJECT_HPP
+
+
+class CodeObject {
+
+};
+
+
+#endif //ROBINOS_CODEOBJECT_HPP
---
+++ Process.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Rian on 9/14/2018.
+//
+
+#include <vector>
+#ifndef ROBINOS_PROCESS_HPP
+#define ROBINOS_PROCESS_HPP
+
+
+class Process{
+private:
+
+};
+
+
+#endif //ROBINOS_PROCESS_HPP
