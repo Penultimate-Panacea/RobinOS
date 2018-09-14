74e20e1d2130bed0de46127a56b27c4e485536c9
Rian <Penultimate-Panacea@users.noreply.github.com>
2018-09-14 10:35:47 -0400

    Added CodeObject skeleton

--- CodeObject.hpp
+++ CodeObject.hpp
@@ -6,8 +6,25 @@
 #define ROBINOS_CODEOBJECT_HPP
 
 
-class CodeObject {
+#include <ostream>
 
+class CodeObject {
+    /*!
+    * Creates an object of processor and I/O operations for simulating the different stages of real world processes
+    */
+private:
+    unsigned int memOps; //Amount of memory needing transfered in bits
+    unsigned int procOps; //Amount of CPU operations needed in instructions
+public:
+    void setMemOps(unsigned int memOps);
+    void setProcOps(unsigned int procOps);
+    unsigned int getMemOps() const;
+    unsigned int getProcOps() const;
+    friend std::ostream &operator<<(std::ostream &os, const CodeObject &object);
+
+    CodeObject(unsigned int memOps, unsigned int procOps);
+
+    virtual ~CodeObject();
 };
 
 
