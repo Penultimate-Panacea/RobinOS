b80f944ebf81d4a87ac6a510f9a1a9b1fabb1440
Rian <Penultimate-Panacea@users.noreply.github.com>
2018-09-22 17:20:48 -0400

    Added Master Controller for managing interactions between classes

---
+++ MasterController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Rian on 9/22/2018.
+//
+
+#include "MasterController.hpp"
---
+++ MasterController.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Rian on 9/22/2018.
+//
+
+#ifndef ROBINOS_MASTERCONTROLLER_HPP
+#define ROBINOS_MASTERCONTROLLER_HPP
+
+
+class MasterController {
+
+};
+
+
+#endif //ROBINOS_MASTERCONTROLLER_HPP
