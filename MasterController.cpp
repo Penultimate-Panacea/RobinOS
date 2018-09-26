528ba683896e49b76c81a08d06fe37e855247137
AlHove <43249047+AlHove@users.noreply.github.com>
2018-09-26 11:36:26 -0400

    Update MasterController.cpp

--- MasterController.cpp
+++ MasterController.cpp
@@ -1,5 +1,30 @@
 //
 // Created by Rian on 9/22/2018.
 //
+//edited by Alyssa 9/22/2018 fleshing out the cpp file more
 
 #include "MasterController.hpp"
+
+unsigned int MasterController::calculateResponseTime(){
+  
+}
+
+unsigned int MasterController::calculateAdvgThroughput(){
+  
+}
+
+void MasterController::setProcess(Process Proc){
+  this.Process = Proc;
+}
+
+void MasterController::setMemory(MemoryManager mem){
+  this.Memory = mem;
+}
+
+MemoryManager MasterController::getMemory(){
+  return this.MemoryManager;
+}
+
+Process MasterController::getProcess(){
+ return this.Process; 
+}
