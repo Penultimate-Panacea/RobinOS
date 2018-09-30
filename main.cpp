99bc803dd8d4133703dfc97ba8015e559fd7ed00
Rian <Penultimate-Panacea@users.noreply.github.com>
2018-09-30 13:31:23 -0400

    Removed redundant CodeObjects

--- main.cpp
+++ main.cpp
@@ -5,10 +5,6 @@
 
 
 int main() {
-    CodeObject simple = CodeObject(15,324);
-    CodeObject bigComp = CodeObject(8192,256);
-    CodeObject bigMem = CodeObject(256, 8192);
-    CodeObject justHuge = CodeObject(12365536, 12365536);
     Processor mainProcessor = Processor();
     MemoryManager mainMemoryManager = MemoryManager();
     mainMemoryManager.populateInitialList();
