27100cc09e8014c0ce6e5f54b498e2674b917bc5
Rian <Penultimate-Panacea@users.noreply.github.com>
2018-09-30 14:08:48 -0400

    Added logging

--- code/main.cpp
+++ code/main.cpp
@@ -4,7 +4,13 @@
 #include "MemoryManager.hpp"
 #include "Scheduler.h"
 
+void loggingStart(){
+    freopen("output2.csv", "w", stdout);
+    freopen("audit.log", "w", stderr);
+}
+
 int main() {
+    loggingStart();
     Processor mainProcessor = Processor();
     MemoryManager mainMemoryManager = MemoryManager();
     mainMemoryManager.populateInitialList();
