fe48baa64e77b67788308034b009fc15d38db4b7
AlHove <43249047+AlHove@users.noreply.github.com>
2018-12-04 17:44:02 -0500

    Update Scheduler.cpp
    quantum 1 works but mathematical issues with 2 and 3

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -1,86 +1,80 @@
-/*
- * Alyssa Hove, Rian Fantozzi, Linh Dang, Dylan Lear
- * 9/19/18
- * Edited to Professor's liking on the 17th of Oct.
- * Nov 26 Editied to turnaround times
- * CS 320 Operating Systems
- * Basic Round Robin
- */
+   /*
+     * Alyssa Hove, Rian Fantozzi, Linh Dang, Dylan Lear
+     * 9/19/18
+     * Edited to Professor's liking on the 17th of Oct.
+     * Nov 26 Editied to turnaround times
+     * CS 320 Operating Systems
+     * Basic Round Robin
+     */
 
-/*! This simulator showcases a basic round robin scheduling system
- * it uses the csv file test.csv to gather the information about the processes
- * only deals with processor time and not I/O time
- * currently only accepts input in the scope of 11 different processes */
+    /*! This simulator showcases a basic round robin scheduling system
+     * it uses the csv file test.csv to gather the information about the processes
+     * only deals with processor time and not I/O time
+     * currently only accepts input in the scope of 11 different processes */
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <algorithm>
+    #include <iostream>
+    #include <fstream>
+    #include <string>
 
-using namespace std;
+    using namespace std;
 
-struct Scheduler{ //makes the structure of scheduler
-    int need_time;
-    int duration;
-    int begin_time;
-    int completed_in;
-    int turnaround;
-    bool complete;
-    bool start;
-};
+    struct Scheduler{ //makes the structure of scheduler
+       unsigned int need_time;
+       unsigned int duration;
+       unsigned int begin_time;
+       unsigned int completed_in;
+       unsigned int turnaround;
+        bool complete;
+        bool start;
+    };
 
-void loggingStart(){
-    freopen("debug.log", "w", stderr);
-    freopen("output.txt", "w", stdout);
-}
-
-void initializer(struct Scheduler* sched) { // initializes the Scheduler
-    for (int i = 0; i < 11; i++)
-    {
-        sched[i].need_time = 0;
-        sched[i].duration = 0;
-        sched[i].begin_time = 0;
-        sched[i].turnaround = 0;
-        sched[i].completed_in =0;
-        sched[i].complete = false;
-        sched[i].start = false;
-
+    void loggingStart(){
+        freopen("debug.log", "w", stderr);
+        freopen("output.txt", "w", stdout);
     }
-}
 
-void parse(Scheduler* sched, string str, int n)
-{
+    void initializer(struct Scheduler* sched) { // initializes the Scheduler
+        for (int i = 0; i < 11; i++)
+        {
+            sched[i].need_time = 0;
+            sched[i].duration = 0;
+            sched[i].begin_time = 0;
+            sched[i].turnaround = 0;
+            sched[i].completed_in =0;
+            sched[i].complete = false;
+            sched[i].start = false;
+
+        }
+    }
+
+void parse(Scheduler* sched, string str, int n){
     std::string::size_type sz;
-    std::string temp[5];
+    std::string temp[2];
     char cstr[str.length()];
     int par_cnt = 0;
-
     str.copy(cstr, str.length()); //copy string into c-style string
     for (int i = 0; i < str.length(); i++) //parse into three string variables, removing commas and spaces
     {
         if (static_cast<int>(cstr[i]) == 44)
         {
             ++par_cnt;
-            continue;
+        continue;
         }
-        else if (static_cast<int>(cstr[i]) == 32)
-            continue;
-        else
-            temp[par_cnt] += cstr[i];
+    else if (static_cast<int>(cstr[i]) == 32)
+        continue;
+    else
+    temp[par_cnt] += cstr[i];
     }
     //convert strings to ints and copy to structure variables
     sched[n].need_time = stoi(temp[0], &sz);
     sched[n].duration = stoi(temp[1], &sz);
-    sched[n].begin_time = stoi(temp[2], &sz);
-    sched[n].turnaround = stoi(temp[3], &sz);
-    sched[n].completed_in = stoi(temp[4], &sz);
-//sched[n].complete = stoi(temp[5], &sz);
- //sched[n].start = stoi(temp[6], &sz);
-}
-bool get_data(Scheduler* scheduler) //read in data from testJobs.txt
+    }
+
+int get_data(struct Scheduler* sched) //read in data from Job.txt, save and parse each line read in
 {
     int n = 0;
     std::string buffer;
+
     std::fstream jobs;
     jobs.open("testJobs.txt", std::fstream::in);
     if (jobs.is_open())
@@ -88,9 +82,10 @@ bool get_data(Scheduler* scheduler) //read in data from testJobs.txt
         while (jobs)
         {
             jobs >> buffer;
-            parse(scheduler, buffer, n);
+            parse(sched, buffer, n);
             n++;
         }
+        jobs.close();
         return 0;
     }
     else
@@ -98,69 +93,57 @@ bool get_data(Scheduler* scheduler) //read in data from testJobs.txt
         std::cout << "error:  file not found" << std::endl;
         return 1;
     }
-    jobs.close();
 }
 
-int find_total(struct Scheduler* scheduler) //calculates total duration of all jobs
-{
-    int total = 0;
-    for (int i = 0; i < 11; i++)
-        total  += scheduler[i].duration;
-    return total;
-}
-float calcAvgResponseTime(int compCount) {
-    float average;
-    average = float(compCount) / float(11.0);
-    return average;
-}
 
-int calcAvgTurnTime(int sum){
-    int avg;
+    float calcAvgNormTurnTime(float sum){
+    float avg;
     avg = sum / 11;
     return avg;
 }
 
-int calcTurnaroundTime(int start, int end) {
-    int result = 0;
+unsigned int calcTurnaroundTime(unsigned int start, unsigned int end) {
+    unsigned int result = 0;
     result = end - start;
     return result;
 }
-float calcNormalTTime(int duration, int tTime){
-    float result;
+ float calcNormalTTime(unsigned int duration, unsigned int tTime){
+        float result;
     result = tTime / duration;
     return result;
 }
 
 void round_robin(struct Scheduler* sched, int ms)//simulates round robin CPU scheduling
 {
-    int time = 0;
-    int diff = 0;
-    int compCount = 0;
+    unsigned int time = 0;
+    unsigned int diff = 0;
+    unsigned int compCount = 0;
     float normalizedTurnTime = 0.0;
-    int sum = 0;
+    unsigned int sum = 0;
+
     do {
         for (int i = 0; i < 11; i++) {
 
-            if (sched[i].need_time <= time && !sched[i].complete) //if job arrived and job not complete
+            if ( !sched[i].complete) //if job arrived and job not complete
             {
                 if (!sched[i].start){ //job will start, set flag to true to indicate start
                     sched[i].begin_time = time;
                     sched[i].start = true;
                 }
 
-
                 if (sched[i].duration <= ms) //if job will complete, set flag to true to indicate job complete
                 {
                     if (sched[i].duration < ms) {
+                        time = time + ms;
                         diff = sched[i].duration;
                         time = time - diff;
-                        time = time + ms;
                         sched[i].completed_in = time;
+                        time = time + diff;
                         sched[i].turnaround = calcTurnaroundTime(sched[i].completed_in, sched[i].begin_time);
-                        sum = sched[i].turnaround + sum;
                         normalizedTurnTime = calcNormalTTime(sched[i].need_time,sched[i].turnaround);
                         cerr << "Job "<< i << " Completed at " << time << " ms. Turnaround time is " << sched[i].turnaround <<
                              ". Normalized turnaround time is " << normalizedTurnTime << endl;
+                        sum = normalizedTurnTime + sum;
                         sched[i].complete = true;
                         compCount++;
                     }
@@ -168,10 +151,10 @@ void round_robin(struct Scheduler* sched, int ms)//simulates round robin CPU sch
                         time = time + ms;
                         sched[i].completed_in = time;
                         sched[i].turnaround = calcTurnaroundTime(sched[i].begin_time, sched[i].completed_in);
-                        sum = sched[i].turnaround + sum;
                         normalizedTurnTime = calcNormalTTime(sched[i].need_time,sched[i].turnaround);
                         cerr << "Job "<< i << " Completed at " << time << " ms. Turnaround time is " << sched[i].turnaround <<
                              ". Normalized turnaround time is " << normalizedTurnTime << endl;
+                        sum = normalizedTurnTime + sum;
                         sched[i].complete = true;
                         compCount++;
                     }
@@ -184,23 +167,25 @@ void round_robin(struct Scheduler* sched, int ms)//simulates round robin CPU sch
             }
         }
     }while (compCount != 11);
-    float reponseTime = calcAvgResponseTime(time);
-    float avgTTime = calcAvgTurnTime(sum);
-    cout << "The average response time was " << reponseTime << "The average Turnaround time is " << avgTTime << endl;
+
+    float avgTTime = calcAvgNormTurnTime(sum);
+
+    cout << "The average Normalized Turnaround time is " << avgTTime << endl;
 }
 
 int main() //driver
 {
     loggingStart();
     Scheduler* object = new Scheduler[11]; //allocate data on heap
-    initializer(object); //initialize variables
-    get_data(object);
-    if (get_data(object)) //open file and read in job data
-        return -1; //returns error if file not opened
+
     cout << "The total ms that the processes will need is " << 298 << "." << endl; // outputs the time needed for all processes
-    for (int i = 1; i < 3; ++i) {
+    for (int i = 1; i < 4; ++i) {
         initializer(object); //initialize variables
+        if (get_data(object)){ //open file and read in job data
+            return -1;
+        }
         cout << "The quantum used is " << i << endl;
+        cout << endl;
         round_robin(object, i); //begin round robin CPU scheduling
     }
 
